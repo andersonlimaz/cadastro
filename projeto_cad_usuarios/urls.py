9a9cea35590ca702939190e07ac7b7d873819df0
andersonlimaz <andersonlimaz@outlook.com>
2023-08-15 13:22:56 -0300

    commit django

---
+++ projeto_cad_usuarios/urls.py
@@ -0,0 +1,11 @@
+
+from django.urls import path
+from app_cad_usuarios import views
+
+urlpatterns = [
+    #rota, view responsavel, nome de referência
+    # usuarios.com 
+    path('',views.home,name='home'),
+    # usuarios.com/usuarios
+    path('usuarios/',views.usuario, name='listagem_usuarios')
+]
