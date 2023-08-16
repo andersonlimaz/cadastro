63b92340f1b01e2a3f8ed1ba991c6524f2792a29
andersonlimaz <andersonlimaz@outlook.com>
2023-08-16 11:21:37 -0300

    tentativa de consertar o django

--- app_cad_usuarios/views.py
+++ app_cad_usuarios/views.py
@@ -1,18 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Usuario
 
 def home(request):
-    return render(request,'usuarios/home.html')
+    return render(request, 'usuarios/home.html')
 
-def usuario(request):
+def usuarios(request):
     if request.method == 'POST':
         novo_usuario = Usuario()
         novo_usuario.nome = request.POST.get('nome')
         novo_usuario.idade = request.POST.get('idade')
         novo_usuario.save()
+        # Depois de salvar o novo usuário, redirecione para a página de listagem de usuários
+        return redirect('listar_usuarios')  # 'listar_usuarios' é o nome da URL
 
+    # Se o método não for POST, apenas exiba a página de listagem de usuários
     usuarios = {
         'usuarios': Usuario.objects.all()
     }
-    
     return render(request, 'usuarios/usuarios.html', usuarios)
--- projeto_cad_usuarios/urls.py
+++ projeto_cad_usuarios/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     # usuarios.com 
     path('',views.home,name='home'),
     # usuarios.com/usuarios
-    path('usuarios/',views.usuario, name='listagem_usuarios')
+    path('usuarios/',views.usuarios, name='listagem_usuarios')
 ]
